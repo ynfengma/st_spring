3793042cbeb5b06afbfa822e76d75591cdf1bfb9
ma <yanfeng.ma@pactera.com>
2022-05-24 17:46:48 +0800

    初始化

---
+++ Spring_all/src/test/java/com/ma/spring/SpringAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ma.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
